eb45f8f2ce0d1c2b1c5dbbc3767a68e0d79f026a
Genji(김수호) <suhokim@coupang.com>
2018-01-21 12:58:55 +0900

    list

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Input, Alert, Rate} from 'antd';
+import {Input, Alert} from 'antd';
 import {Button, Slider} from './index';
 
 export default class App extends React.Component {
@@ -9,7 +9,8 @@ export default class App extends React.Component {
       text: '',
       textLength: 0,
       loading: false,
-      warning: 'none'
+      warning: 'none',
+      list: []
     };
     this.maximumNumber = 15;
   }
@@ -25,26 +26,37 @@ export default class App extends React.Component {
 
     this.setState({ text, textLength, warning: 'none' });
   }
+  onSubmitHandler(e) {
+    e.preventDefault();
+    this.setState(state => {
+      return {
+        list: [...state.list, state.text],
+        text: ''
+      }
+    });
+  }
   render() {
     return (
       <div>
         <div>
           ({this.state.textLength}/{this.maximumNumber})
         </div>
-        <Input.Search type="text"
-                      onChange={(e) => this.onChangeHandler(e)}
-                      value={this.state.text}
-        />
+        <form onSubmit={(e) => this.onSubmitHandler(e)}>
+          <Input.Search type="text"
+                        onChange={(e) => this.onChangeHandler(e)}
+                        value={this.state.text}
+          />
+        </form>
 
         <Alert style={{display: this.state.warning}} message={'15자 까지만 입력가능합니다!!!'} type="error" />
 
+        <ul>
+          {this.state.list.map((v, i) => (<li key={i}>{v}</li>))}
+        </ul>
         <div>
           <Button type="primary" onClick={(e) => console.log(e)} />
           <Button onClick={(e) => console.log(e)} />
           <Slider/>
-        </div>
-        <div>
-          <Rate character={this.state.text} allowHalf={true} count={10} defaultValue={4.5} />
         </div>
       </div>
     );
