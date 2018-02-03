81c8d8939ee03701467a57f1149807d720633e4e
Genji(김수호) <suhokim@coupang.com>
2018-02-04 00:44:25 +0900

    view template

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import {connect} from 'react-redux';
 import 'antd-mobile/dist/antd-mobile.css';
 import './App.css';
-import {NavBar, Drawer, Icon, List} from 'antd-mobile';
+import {NavBar, Drawer, Icon, List, WhiteSpace, WingBlank, Button, Flex, Card, Carousel} from 'antd-mobile';
 
 class App extends React.Component {
   constructor(props) {
@@ -14,33 +14,174 @@ class App extends React.Component {
   }
 
   render() {
-    const sidebar = (<List>
-      {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((i, index) => {
-        if (index === 0) {
-          return (<List.Item key={index}
-                             thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
-                             multipleLine
-          >Category</List.Item>);
-        }
-        return (<List.Item key={index}
-                           thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
-        >Category{index}</List.Item>);
-      })}
-    </List>);
+    const sidebar = (
+      <div>
+        <List>
+          <List.Item key={0}
+                     thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
+                     multipleLine
+          >이용방법</List.Item>
+          <List.Item key={1}
+                     thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
+                     multipleLine
+          >고객센터</List.Item>
+          <List.Item key={2}
+                     thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
+                     multipleLine
+          >공지사항</List.Item>
+        </List>
+        <WhiteSpace size="lg" />
+        <WingBlank>
+          <Button type="ghost">멤버십신청</Button>
+        </WingBlank>
+        <WhiteSpace size="lg" />
+        <WingBlank>
+          <Flex>
+            <Flex.Item>
+              <Button size="small">블로그</Button>
+            </Flex.Item>
+            <Flex.Item>
+              <Button size="small">페이스북</Button>
+            </Flex.Item>
+          </Flex>
+        </WingBlank>
+      </div>
+    );
 
     return (
       <div>
-        <NavBar icon={<Icon type="ellipsis" />} onLeftClick={()=>this.props.toggle()}>Basic</NavBar>
+        <NavBar icon={<Icon type="ellipsis" />} onLeftClick={()=>this.props.toggle()}>TLX</NavBar>
         <Drawer
           className="my-drawer"
           style={{ minHeight: document.documentElement.clientHeight }}
-          enableDragHandle
-          contentStyle={{ color: '#A6A6A6', textAlign: 'center', paddingTop: 42 }}
+          contentStyle={{ color: '#A6A6A6', textAlign: 'center', paddingTop: 15 }}
           sidebar={sidebar}
           open={this.props.sidebar.opened}
           onOpenChange={(e) => this.onOpenChange(e)}
         >
-          Click upper-left corner
+          <div className="flex-container">
+            <Flex>
+              <Flex.Item>
+                <Carousel className="space-carousel"
+                          frameOverflow="visible"
+                          cellSpacing={10}
+                          slideWidth={0.8}
+                          autoplay
+                          infinite
+                          beforeChange={(from, to) => (from + to)}
+                          afterChange={index => this.setState({ slideIndex: index })}
+                >
+                  <a
+                    href="http://www.alipay.com"
+                    style={{
+                      display: 'block',
+                      position: 'relative',
+                      boxShadow: '2px 1px 1px rgba(0, 0, 0, 0.2)',
+                    }}
+                  >
+                    <img
+                      src="https://tlxcdn.s3.amazonaws.com/images/bg/shutterstock_267830291_.jpg"
+                      alt=""
+                      style={{ width: '100%', verticalAlign: 'top' }}
+                      onLoad={() => {
+                        // fire window resize event to change height
+                        window.dispatchEvent(new Event('resize'));
+                        this.setState({ imgHeight: 'auto' });
+                      }}
+                    />
+                  </a>
+                  <a
+                    href="http://www.alipay.com"
+                    style={{
+                      display: 'block',
+                      position: 'relative',
+                      boxShadow: '2px 1px 1px rgba(0, 0, 0, 0.2)',
+                    }}
+                  >
+                    <img
+                      src="https://tlxcdn.s3.amazonaws.com/images/bg/shutterstock_269242565.jpg"
+                      alt=""
+                      style={{ width: '100%', verticalAlign: 'top' }}
+                      onLoad={() => {
+                        // fire window resize event to change height
+                        window.dispatchEvent(new Event('resize'));
+                        this.setState({ imgHeight: 'auto' });
+                      }}
+                    />
+                  </a>
+                  <a
+                    href="http://www.alipay.com"
+                    style={{
+                      display: 'block',
+                      position: 'relative',
+                      boxShadow: '2px 1px 1px rgba(0, 0, 0, 0.2)',
+                    }}
+                  >
+                    <img
+                      src="https://tlxcdn.s3.amazonaws.com/images/bg/shutterstock_269207801.jpg"
+                      alt=""
+                      style={{ width: '100%', verticalAlign: 'top' }}
+                      onLoad={() => {
+                        // fire window resize event to change height
+                        window.dispatchEvent(new Event('resize'));
+                        this.setState({ imgHeight: 'auto' });
+                      }}
+                    />
+                  </a>
+                </Carousel>
+              </Flex.Item>
+            </Flex>
+          </div>
+          <div>
+            <Flex>
+              <Flex.Item>
+                <Card full>
+                  <Card.Header
+                    title="This is title"
+                    thumb="https://cloud.githubusercontent.com/assets/1698185/18039916/f025c090-6dd9-11e6-9d86-a4d48a1bf049.png"
+                    extra={<span>this is extra</span>}
+                  />
+                  <Card.Body>
+                    <div>This is content of `Card`</div>
+                  </Card.Body>
+                  <Card.Footer content="footer content" extra={<div>extra footer content</div>} />
+                </Card>
+              </Flex.Item>
+            </Flex>
+            <WhiteSpace size="lg" />
+            <Flex>
+              <Flex.Item>
+                <Card full>
+                  <Card.Header
+                    title="This is title"
+                    thumb="https://cloud.githubusercontent.com/assets/1698185/18039916/f025c090-6dd9-11e6-9d86-a4d48a1bf049.png"
+                    extra={<span>this is extra</span>}
+                  />
+                  <Card.Body>
+                    <div>This is content of `Card`</div>
+                  </Card.Body>
+                  <Card.Footer content="footer content" extra={<div>extra footer content</div>} />
+                </Card>
+              </Flex.Item>
+            </Flex>
+            <WhiteSpace size="lg" />
+            <Flex>
+              <Flex.Item>
+                <Card full>
+                  <Card.Header
+                    title="This is title"
+                    thumb="https://cloud.githubusercontent.com/assets/1698185/18039916/f025c090-6dd9-11e6-9d86-a4d48a1bf049.png"
+                    extra={<span>this is extra</span>}
+                  />
+                  <Card.Body>
+                    <div>This is content of `Card`</div>
+                  </Card.Body>
+                  <Card.Footer content="footer content" extra={<div>extra footer content</div>} />
+                </Card>
+              </Flex.Item>
+            </Flex>
+            <WhiteSpace size="lg" />
+          </div>
         </Drawer>
       </div>
     );
