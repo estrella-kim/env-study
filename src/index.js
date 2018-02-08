b45ba59b46ee5f1b463ed900c2d456cab70c53a2
estrella-kim <puris9030@gmail.com>
2018-02-08 19:18:49 +0900

    add comment during review

--- src/index.js
+++ src/index.js
@@ -14,8 +14,9 @@ const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
-
 ReactDOM.render(
-  <Provider store={store}>
+ <Provider store={store}>
     <App />
-  </Provider>, document.getElementById('app'));
+  </Provider>, document.getElementById('app') //provider는 app의 store를 connect할 수 있도록 provide 한다. connect와 함께 쓰인다.
+
+);
